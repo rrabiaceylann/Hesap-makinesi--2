28310b34ef10d9bcf6a0f59fd0467c39e15c0648
Rabia <rabiya_87@hotmail.com>
2025-07-20 20:13:15 +0300

    Add files via upload

---
+++ hesap makinesi/hesap makinesi/Program.cs
@@ -0,0 +1,63 @@
+﻿
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan ilk sayıyı al
+            Console.Write("1. sayıyı giriniz: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            // Kullanıcıdan ikinci sayıyı al
+            Console.Write("2. sayıyı giriniz: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            // Kullanıcıdan işlem seçmesini iste
+            Console.WriteLine("Yapmak istediğiniz işlemi seçin:");
+            Console.WriteLine("Toplama için +");
+            Console.WriteLine("Çıkarma için -");
+            Console.WriteLine("Çarpma için *");
+            Console.WriteLine("Bölme için /");
+            Console.Write("İşlem: ");
+            string islem =Console.ReadLine();
+
+            // İşlemi gerçekleştir
+            if (islem == "+")
+            {
+                int sonuc = sayi1 + sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            else if (islem == "-")
+            {
+                int sonuc = sayi1 - sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            else if (islem == "*")
+            {
+                int sonuc = sayi1 * sayi2;
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            else if (islem == "/")
+            {
+                // Sıfıra bölme kontrolü
+                if (sayi2 != 0)
+                {
+                    double sonuc = (double)sayi1 / sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                }
+                else
+                {
+                    Console.WriteLine("Hata: Bir sayıyı sıfıra bölemezsiniz!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz işlem seçtiniz.");
+            }
+
+            Console.WriteLine("\nDevam etmek için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
